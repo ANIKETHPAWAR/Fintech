bf01d62615d4bca8c9fe9fab4f1d17d6ff66913e
ANIKETHPAWAR <Anikethpawar05@gmail.com>
2025-05-27 23:52:37 +0530

    account schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -63,5 +63,21 @@ userSchema.methods.comparePassword = async function(candidatePassword){
 } 
 
 
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+const Account = mongoose.model('Account', accountSchema);
 const User = mongoose.model('User',userSchema);
-module.exports = User
+
+
+
+module.exports = {User,Account};
