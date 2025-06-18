22d35b96489039d3a3ede7d82671e6a471dcccd4
ANIKETHPAWAR <Anikethpawar05@gmail.com>
2025-06-18 21:38:58 +0530

    signup+signin frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Signup } from './pages/signup';
+import { Signin } from './pages/signin';
 function App() {
-
   return (
     <div>
-        Hello world
+      <Router>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
